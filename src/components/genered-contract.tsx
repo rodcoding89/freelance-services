3bee4711d4ccab4771dad18f24b2611bf11973e0
rod coding <rodcoding@gmail.com>
2025-04-28 20:42:11 +0200

    continue with dynamic contract generation

--- src/components/genered-contract.tsx
+++ src/components/genered-contract.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { useTranslationContext } from "@/hooks/app-hook";
-import { PDFDocument } from "pdf-lib";
-import { useEffect } from "react";
+
+import { useEffect, useState } from "react";
+import InitCanvaSignature from "./initCanvaSignature";
 
 interface GeneredContractProps{
     locale:string
@@ -10,10 +11,21 @@ interface GeneredContractProps{
 
 const GeneredContract:React.FC<GeneredContractProps> = ({locale})=>{
     const t:any = useTranslationContext();
+    const [signingLink, setSigningLink] = useState<string | null>(null);
+    const handleSignatureChange = (data:any)=>{
+        console.log("data",data)
+        setSigningLink(data)
+    }
     useEffect(()=>{
         
     },[])
-    return null
+    return (
+        <div>
+            <section className="signing">
+                <InitCanvaSignature locale={locale} emit={handleSignatureChange}/>
+            </section>
+        </div>
+    )
 }
 
 export default GeneredContract
